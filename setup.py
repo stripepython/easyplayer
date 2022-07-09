482108f63dc4fc6e1774f44c01e4068b0988a2e1
stripepython <98206176+stripepython@users.noreply.github.com>
2022-07-09 15:27:33 +0800

    Update requirements.txt and setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,9 @@
-from setuptools import setup, find_packages
+from setuptools import setup
 
 with open('README.md', 'r', encoding='utf-8') as f:
     long_description = f.read()
+with open('requirements.txt', 'r', encoding='utf-8') as f:
+    requirements = f.read().split('\n')
 
 setup(
     name='easyplayer',
@@ -10,7 +12,7 @@ setup(
     maintainer='stripe-python',
     maintainer_email='stripe-python@139.com',
     py_modules=['easyplayer'],
-    version='0.1.2',
+    version='0.2.1',
     description='Easyplayer is a python library that encapsulates the complex API of pygame2 to help users build games faster.',
     long_description=long_description,
     long_description_content_type='text/markdown',
@@ -19,12 +21,8 @@ setup(
         'License :: OSI Approved :: MIT License',
         'Operating System :: OS Independent',
     ],
-    install_requires=['pygame==2.1.2', 'click~=8.0.3', 'numpy~=1.21.0',
-                      'opencv-python~=4.5.2.54', 'tqdm~=4.47.0', 'pydub~=0.24.1',
-                      'pillow~=8.3.0', 'requests~=2.25.1', 'aiml~=0.9.2',
-                      'pinyin2hanzi~=0.1.1', 'pyttsx3~=2.90', 'future~=0.18.2',
-                      'dataclasses~=0.6'],
     python_requires='>=3.6',
     url='https://github.com/stripepython/easyplayer',
     download_url='https://github.com/stripepython/easyplayer',
+    install_requires=requirements,
 )
